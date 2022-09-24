bda273f9af7c8e0d625e4a3d51ab1bb172a4c54c
Joel <joel@Joels-MacBook-Pro.local>
2022-09-24 07:37:01 -0500

    Made the projects tab easier to look at

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-
+    display:flex;
+    flex-flow: column;
+    align-items:center;
+    text-align:center;
+    margin-top:15px;
 `
 const Image = styled.img`
     border: 5px solid black;
@@ -10,12 +14,29 @@ const Image = styled.img`
     width: 275px;
     border-radius:25px;
 `
+const Title = styled.p`
 
+font-size:20px;
+margin:2px;
+font-family: 'Montserrat', sans-serif;
+
+`
+const Description = styled.p`
+    font-size:10px;
+    margin:2px;
+    font-family: 'Montserrat', sans-serif;
+
+`
+const GoTo = styled.a`
+text-decoration:none;
+color:black;
+`
 function Project(props) {
     return(
         <Wrapper>
             <Image src={props.img}/>
-            text
+            <GoTo href = {props.link}><Title>{props.name}</Title></GoTo>
+            <Description>{props.description}</Description>      
         </Wrapper>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import Project from './Project.js'
 import ETHFeesImg from '../images/ETHFees.jpg'
+import ConsensusTaxImg from '../images/consensustax.jpg'
+
 const ProjectHolder = styled.div`
 
 `
 function Projects() {
     return(
         <ProjectHolder>
-            <Project name = "ETH Fees" img = {ETHFeesImg}/>
+            <Project name = "ETH Fees" img = {ETHFeesImg} description = "Enter a wallet address to find out unique insights" link = "https://ethfees.xyz"/>
+            <Project name = "Consensus Tax" img = {ConsensusTaxImg} description = {`Find out your specfic tax needs at one click.\n Link soon.`} link = "https://ethfees.xyz"/>
         </ProjectHolder>
     )
 }
